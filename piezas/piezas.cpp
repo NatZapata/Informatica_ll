8559cdfa2f991e9743c3f6739c8cc79631ebb261
ValentinaPosada2005 <valentina.posadav@udea.edu.co>
2026-03-12 15:46:46 -0500

    Piezas 1
    funciones para la creación de las piezas 1

---
+++ piezas/piezas.cpp
@@ -0,0 +1,99 @@
+#include "piezas.h"
+
+void crear_pieza_aleatoria(unsigned char **matriz,unsigned short &ancho_matriz,unsigned char *primera_mascara,
+                           unsigned char *segunda_mascara, unsigned char *tercera_mascara,
+                           unsigned char *cuarta_mascara){
+    srand(time(0));
+    unsigned char numero_pieza= (rand()%5)+1;
+    unsigned short mitad_fila=ancho_matriz/2;
+
+    for(unsigned short i = 0; i < ancho_matriz; i++){
+        primera_mascara[i] = 0;
+        segunda_mascara[i] = 0;
+        tercera_mascara[i] = 0;
+        cuarta_mascara[i] = 0;
+    }
+
+
+    switch (numero_pieza) {
+        //Forma de linea
+    case 1:{
+        for(unsigned short i=0;i< ancho_matriz;i++){
+            if(i==mitad_fila){
+                *(primera_mascara+i)=1;
+                *(segunda_mascara+i)=1;
+                *(tercera_mascara+i)=1;
+                *(cuarta_mascara+i)=1;
+
+            }
+
+        }
+
+        break;
+    }
+    //Forma de cuadradro macizo
+    case 2:{
+        for(unsigned short i=0;i< ancho_matriz;i++){
+            if(i==mitad_fila ||  i==mitad_fila+1){
+                *(primera_mascara+i)=1;
+                *(segunda_mascara+i)=1;
+                // *(tercera_mascara+i)=0;
+                // *(cuarta_mascara+i)=0;
+
+            }
+        }
+        break;
+    }
+    case 3:{
+        //Forma de T
+        for(unsigned short i=0;i< ancho_matriz;i++){
+            if(i==mitad_fila || i==mitad_fila-1 || i==mitad_fila+1){
+                *(primera_mascara+i)=1;
+                //*(tercera_mascara+i)=0;
+                //*(cuarta_mascara+i)=0;
+                if(i==mitad_fila){
+                    *(segunda_mascara+i)=1;
+                }
+
+            }
+        }
+
+
+        break;
+    }
+    case 4:{
+        //Forma de L
+        for(unsigned short i=0;i< ancho_matriz;i++){
+            if(i==mitad_fila){
+                *(primera_mascara+i)=1;
+                *(segunda_mascara+i)=1;
+                *(tercera_mascara+i)=1;
+                // *(cuarta_mascara+i)=0;
+
+            } else if(i==mitad_fila-1){
+                *(tercera_mascara+i)=1;
+
+            }
+        }
+        break;
+
+    }
+    default:{
+        for(unsigned short i=0;i< ancho_matriz;i++){
+            if(i==mitad_fila){
+                *(primera_mascara+i)=1;
+                *(segunda_mascara+i)=1;
+                *(tercera_mascara+i)=1;
+                //  *(cuarta_mascara+i)=0;
+
+            } else if(i==mitad_fila+1){
+                *(tercera_mascara+i)=1;
+
+            }
+        }
+        break;
+    }
+
+
+    }
+}
---
+++ piezas/piezas.h
@@ -0,0 +1,10 @@
+#ifndef PIEZAS_H
+#define PIEZAS_H
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+void crear_pieza_aleatoria(unsigned char **matriz,unsigned short &ancho_matriz,unsigned char *primera_mascara,
+                           unsigned char *segunda_mascara, unsigned char *tercera_mascara,
+                           unsigned char *cuarta_mascara);
+#endif // PIEZAS_H
